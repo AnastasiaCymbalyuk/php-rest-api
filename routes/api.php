96b3d99676147135ec78d481596b39a7cfa63be6
AnastasiaCymbalyuk <n.wolk@mail.ru>
2023-06-26 21:44:19 +0300

    fixed

---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->post('/tokens/create',function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['error' => 'user not fount', 401]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('/cars', App\Http\Controllers\CarsController::class);
+});
+
